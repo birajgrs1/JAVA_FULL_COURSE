e52e13a9dbece921b9f5183df6aae6e670854a71
birajgrs <birajghorasaine11@gmail.com>
2024-12-01 19:44:17 +0545

    awt  completed and layout mgmt started ...

---
+++ src/forMyOwnPractice/AbstractWindowingToolkit_AWT_in_Java/DialogBoxes/FileDialogBoxes/FileDialogDemo.java
@@ -0,0 +1,17 @@
+package forMyOwnPractice.AbstractWindowingToolkit_AWT_in_Java.DialogBoxes.FileDialogBoxes;
+import java.awt.*;
+
+public class FileDialogDemo {
+    public static void main(String[] args) {
+        Frame frame = new Frame("File Dialog Example");
+
+        FileDialog fileDialog = new FileDialog(frame, "Open File", FileDialog.LOAD);
+        fileDialog.setVisible(true);
+
+        System.out.println("Selected File: " + fileDialog.getFile());
+        System.out.println("Selected Directory: " + fileDialog.getDirectory());
+
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+    }
+}
---
+++ src/forMyOwnPractice/AbstractWindowingToolkit_AWT_in_Java/MenuInAWT/MainProgram.java
@@ -0,0 +1,7 @@
+package forMyOwnPractice.AbstractWindowingToolkit_AWT_in_Java.MenuInAWT;
+
+public class MainProgram {
+    public static void main(String[] args) {
+        MenuDemo demo = new MenuDemo();
+    }
+}
